3cc88d957f6e7b575f954efcfa05e3b50d22624c
Guillermo Nunez <guillenunez.ds@gmail.com>
2023-09-20 11:19:53 -0300

    App.py: Se agrega botón Calcular y se modifica comportamiento (solo funciona si se presiona el boton). Se mejoran inputs del formulario

--- app/app.py
+++ app/app.py
@@ -6,14 +6,20 @@ st.info("Esta aplicación fue creada solo para propósitos educativos.")
 
 
 # Diccionario con los inputs
-dic_input = {
-    'age': 69.0,
-    'karnofsky_score': 60.0,
-    'months_from_diagnosis': 7.0,
-    'prior_therapy': 'No',
-    'treatment': 'Standard',
-    'celltype': 'Squamous'
-}
+## Para variables numéricas utilizar st.number_input
+## Para variables categóricas, extraer todos los valores posibles desde el encoder (encoder.categories_)
+### Las categorías del encoder se ven en los otros notebooks
+
+with st.form("user_input"):     # Esto es para que las predicciones ocurran solo cuando le damos clic al botón submit y no cada vez que actualizamos un parámetro
+    dic_input = {
+        'age': st.number_input("Edad", min_value=0, max_value=100, value=50),
+        'karnofsky_score': st.number_input("Karnofsky Score", min_value=0, max_value=100, value=60),
+        'months_from_diagnosis': st.number_input("Meses desde diagnostico", min_value=0, max_value=100, value=0),
+        'prior_therapy': st.selectbox("Terapia previa",['No', 'Yes'] ),
+        'treatment': st.selectbox("Tratamiento", ['Standard', 'Test']),
+        'celltype': st.selectbox("Tipo de célula", ['Adeno', 'Large', 'Smallcell', 'Squamous'])
+    }
+    submit = st.form_submit_button("Calcular")     # Acá se crea el botón Submit
 
 
 # Importando librería para cargar modelo y encoder
@@ -33,8 +39,9 @@ import sys
 sys.path.append("..")
 import data_utils
 
-survival_prob, time = data_utils.predict_survival_probability(dic_input, model, encoder)
-diagnosis = f"El paciente sobrevivirá `{time} meses` con una probabilidad de `{survival_prob}%`"
+if submit:      # Este if es para que "si se presiona el botón submit (Calcular), se ejecute la función y haga la predicción, de lo contrario no hace nada"
+    survival_prob, time = data_utils.predict_survival_probability(dic_input, model, encoder)
+    diagnosis = f"El paciente sobrevivirá `{time} meses` con una probabilidad de `{survival_prob}%`"
 
-st.write(diagnosis)
+    st.write(diagnosis)
 
